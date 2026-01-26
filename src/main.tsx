0f66a632fe1d256e073418a7e475d39165833502
HARIKRISHNAN <185481537+Harikrishnan3020@users.noreply.github.com>
2026-01-26 18:29:34 +0530

    docs: add header to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,8 @@
+/**
+ * Application Entry Point
+ * Mounts the React application to the DOM.
+ */
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
