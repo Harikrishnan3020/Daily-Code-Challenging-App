0d56b824146b507109d23b064696d973c7ff0ce1
HARIKRISHNAN <185481537+Harikrishnan3020@users.noreply.github.com>
2026-01-26 18:32:16 +0530

    refactor: explicit return type for cn utility

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,7 +8,7 @@ import { twMerge } from "tailwind-merge";
  * @param inputs - List of class names or conditional class objects
  * @returns Merged class string
  */
-export function cn(...inputs: ClassValue[]) {
+export function cn(...inputs: ClassValue[]): string {
   return twMerge(clsx(inputs));
 }
 
