ca85522fd190090804913cb9c61dd4b7486f47dc
HARIKRISHNAN <185481537+Harikrishnan3020@users.noreply.github.com>
2026-02-02 21:20:03 +0530

    docs: add file header to vite-env.d.ts

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,3 +1,8 @@
+/**
+ * File: vite-env.d.ts
+ * Description: Implementation of vite-env.
+ * Copyright: 2026 Daily Code Challenge
+ */
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
